b4488eb3acb183451372aef5695ab57fa0cd5160
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-20 04:27:43 +0800

    added an API for members and dummy data for testing

---
+++ app/api/db.ts
@@ -0,0 +1,7 @@
+const members = [
+    { id: '1', name: 'John Doe', age: 30 },
+    { id: '2', name: 'Jane Doe', age: 25 },
+    { id: '3', name: 'Jim Beam', age: 35 },
+];
+
+export default members;
