aec9415cd82b1ba137c593e5a06785e033505c7c
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-20 05:38:25 +0800

    tested a fetching to display data to the UI

--- app/(dashboard)/dashboard/members/page.tsx
+++ app/(dashboard)/dashboard/members/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+const Members = async () => {
+  const response = await fetch("http://localhost:3000/api/members");
+  const members = await response.json();
 
-const Members = () => {
   return (
     <main className="bg-black/60 px-10 h-screen">
       <h1>Members</h1>
+      <code>{JSON.stringify(members, null, 2)}</code>
     </main>
   )
 }
