1442f39d7b771810a82b157b0fe2d2c192a34c4b
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-21 07:01:33 +0800

    added design for aside buttons

--- components/Dashboard/Aside.tsx
+++ components/Dashboard/Aside.tsx
@@ -1,10 +1,23 @@
+'use client';
+
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation"
+
 const Aside = () => {
+  const pathname = usePathname();
+
   return (
     <aside className="bg-[#182920]">
-      <nav className="flex-1 p-2 space-y-2">
-        <a href="/dashboard" className="block p-2 rounded-lg hover:bg-gray-100">Home</a>
-        <a href="/dashboard/members" className="block p-2 rounded-lg hover:bg-gray-100">Members</a>
-        <a href="/dashboard/lifegroups" className="block p-2 rounded-lg hover:bg-gray-100">Life Groups</a>
+      <nav className="flex-1 flex flex-col p-2 space-y-2">
+        <a href="/dashboard" className={cn("aside_button", { "aside_button_active": pathname === "/dashboard" })}>
+          Home
+        </a>
+        <a href="/dashboard/members" className={cn("aside_button", { "aside_button_active": pathname === "/dashboard/members" })}>
+          Members
+        </a>
+        <a href="/dashboard/lifegroups" className={cn("aside_button", { "aside_button_active": pathname === "/dashboard/lifegroups" })}>
+          Life Groups
+        </a>
       </nav>
     </aside>
   )
