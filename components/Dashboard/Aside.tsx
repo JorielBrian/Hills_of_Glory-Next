896c895d24e4c387c8ed52e9f54bac602ea65cdc
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-20 15:21:33 +0800

    made my dashboard mobile friendly

--- components/Dashboard/Aside.tsx
+++ components/Dashboard/Aside.tsx
@@ -1,11 +1,10 @@
 const Aside = () => {
   return (
     <aside className="bg-[#182920]">
-      <nav className="flex-1 p-4 space-y-2">
-        <a href="#" className="block p-2 rounded-lg hover:bg-gray-100">Home</a>
-        <a href="#" className="block p-2 rounded-lg hover:bg-gray-100">Members</a>
-        <a href="#" className="block p-2 rounded-lg hover:bg-gray-100">Events</a>
-        <a href="#" className="block p-2 rounded-lg hover:bg-gray-100">Settings</a>
+      <nav className="flex-1 p-2 space-y-2">
+        <a href="/dashboard" className="block p-2 rounded-lg hover:bg-gray-100">Home</a>
+        <a href="/dashboard/members" className="block p-2 rounded-lg hover:bg-gray-100">Members</a>
+        <a href="/dashboard/lifegroups" className="block p-2 rounded-lg hover:bg-gray-100">Life Groups</a>
       </nav>
     </aside>
   )
--- components/Dashboard/Header.tsx
+++ components/Dashboard/Header.tsx
@@ -3,8 +3,9 @@ const Header = () => {
     <header className="bg-[#d9dfbc]">
         <a href="/">
             <div className="flex text-shadow-lg">
-                <img src="/hog_logo.png" alt="logo" className="size-15" />
-                <h1 className="px-1 text-3xl text-[#fdc53a] font-bold content-center">Hills of Glory</h1><h1 className="text-3xl content-center px-1">Mabalacat</h1>
+                <img src="/hog_logo.png" alt="logo" className="size-8 2xl:size-10" />
+                <h1 className="px-1 text-lg 2xl:text-4xl text-[#fdc53a] font-bold content-center">Hills of Glory</h1>
+                <h1 className="px-1 text-lg 2xl:text-4xl content-center">Mabalacat</h1>
             </div>
         </a>
     </header>
