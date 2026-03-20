de5b2eeae4bf338063006b4a2cbcbd4031358695
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-20 11:23:09 +0800

    temporarily removed parameters

--- app/(dashboard)/dashboard/members/[id]/page.tsx
+++ app/(dashboard)/dashboard/members/[id]/page.tsx
@@ -1,8 +1,9 @@
-const Member = async ({ params }: {params: Promise<{id: string}>}) => {
-    const { id } = await params;
+// const Member = async ({ params }: {params: Promise<{id: string}>}) => {
+//     const { id } = await params;
+const Member = () => {
   return (
     <main className="bg-black/60 px-10 h-screen">
-      <h1>Member: {id}</h1>
+      <h1>Member:</h1>
     </main>
   )
 }
