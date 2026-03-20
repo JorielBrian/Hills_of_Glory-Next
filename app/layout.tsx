383cdda99de95fb767a75e23a7299c7d36152b1b
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-20 14:41:07 +0800

    bring back the main layout

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import Header from "@/components/Header";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/app/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Hills of Glory - Mabalacat",
+  description: "Web application for Hills of Glory Mabalacat",
+  icons: {
+    icon: "/hog_logo.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full">
+        {children}
+      </body>
+    </html>
+  );
+}
