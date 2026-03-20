511f9e82e904bc537d1e9fdf568ece87fb2054d6
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-20 11:16:07 +0800

    added a children to view pages

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,10 @@
-const Layout = () => {
+import Header from "@/components/Header"
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="bg-black/50 h-screen">
-        <header className="flex w-full justify-between px-15 py-5 bg-black/70">
-            <a href="/">
-                <div className="flex">
-                    <img src="/hog_logo.png" alt="logo" className="size-15" />
-                    <h1 className="text-3xl text-[#fdc53a] content-center px-1">Hills of Glory</h1><h1 className="text-3xl content-center px-1">Mabalacat</h1>
-                </div>
-            </a>
-            <a href="/dashboard" className="inline-block text-xl hover:underline leading-normal" > Dashboard </a>
-        </header>
+        <Header />
+          {children}
     </main>
   )
 }
