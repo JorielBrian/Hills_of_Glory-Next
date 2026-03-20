fc5d55c3917ce1ff233a1ccc747a493ae9d673e4
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-20 15:01:27 +0800

    made the home page mobile friendly

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,17 +2,17 @@ const Home = () => {
   return (
     <main className="w-full">
         <section>
-            <h1 className="text-6xl p-8 w-1/2">TO HONOR GOD AND TO RAISE COMMITTED DISCIPLES</h1>
-            <h2 className="h-fit text-4xl text-[#fdc53a] px-10">Who are able to discple multitudes</h2>
+            <h1 className="text-xl 2xl:text-6xl p-8 2xl:w-1/2">TO HONOR GOD AND TO RAISE COMMITTED DISCIPLES</h1>
+            <h2 className="h-fit text:lg 2xl:text-4xl text-[#fdc53a] px-10">Who are able to discple multitudes</h2>
         </section>
-        <section className="flex w-1/2 p-20 mt-50">
+        <section className="flex mt-20 2xl:w-1/2 2xl:p-20 2xl:mt-50">
             <div>
-                <h1 className="text-3xl py-3 text-white font-[Century Gothic] font-bold">Announcements</h1>
+                <h3 className="2xl:text-3xl py-3 text-white font-[Century Gothic] font-bold">Announcements</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia doloribus fuga autem vel quasi porro. Placeat, quibusdam officia dolore culpa repudiandae doloribus eius doloremque ratione sequi cupiditate nostrum asperiores porro?</p>
             </div>
             <div className="border border-white ml-5 mr-15"></div>
             <div>
-                <h1 className="text-3xl py-3 text-white font-[Century Gothic] font-bold">Upcoming Events</h1>
+                <h3 className="2xl:text-3xl py-3 text-white font-[Century Gothic] font-bold">Upcoming Events</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia ab, hic sint ratione ea voluptatem aut veniam aperiam pariatur. Tenetur eum dolore reiciendis veritatis illum, dolores aut numquam quibusdam velit.</p>
             </div>
         </section>
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="flex flex-col 2xl:flex-row w-full justify-between px-15 py-5">
         <a href="/">
-            <div className="flex 2xl:text-3xl">
+            <div className="flex text-xl 2xl:text-3xl">
                 <img src="/hog_logo.png" alt="logo" className="size-10 2xl:size-15" />
                 <h1 className="text-[#fdc53a] content-center px-1">Hills of Glory</h1><h1 className="content-center px-1">Mabalacat</h1>
             </div>
