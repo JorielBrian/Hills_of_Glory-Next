cac67f9e8340c6386bb0891733775eee00874430
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-20 13:56:32 +0800

    added shadow in text for Hills of Glory name

--- components/Dashboard/Header.tsx
+++ components/Dashboard/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="bg-[#d9dfbc]">
         <a href="/">
-            <div className="flex">
+            <div className="flex text-shadow-lg">
                 <img src="/hog_logo.png" alt="logo" className="size-15" />
                 <h1 className="px-1 text-3xl text-[#fdc53a] font-bold content-center">Hills of Glory</h1><h1 className="text-3xl content-center px-1">Mabalacat</h1>
             </div>
