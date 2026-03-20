c8bec116388655c81f60e06a021f035c14c59c0b
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-20 14:54:50 +0800

    made the root layout mobile first design

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="bg-[url(/background_one.jpg)] bg-cover">
+    <main className="bg-[url(/background_one.jpg)] bg-cover bg-fixed">
       <div className="p-5 bg-black/60">
         <Header />
         {children}
