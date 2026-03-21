39eb7e1904866597736ccbfc8902a544f431415f
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-21 23:34:47 +0800

    added a content for the dashboard page

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,28 @@
 const Dashboard = () => {
   return (
-    <main className="h-screen">
-      <h1>Welcome to Dashboard</h1>
+    <main className="dashboard_main">
+      <section className="bg-white font-bold">
+          <h1 className="text-6xl p-10">Dashboard</h1>
+      </section>
+      <section className="bg-white p-2">
+          <div className="grid grid-cols-3 grid-rows-2 gap-4 p-2 *:h-20">
+              <div className="col-span-2 row-span-1 relative overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                  <h1>Service Attendance</h1>
+              </div>
+              <div className="col-span-1 row-span-1 relative overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                  <h1>Life Guides</h1>
+              </div>
+              <div className="col-span-1 row-span-1 relative overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                  <h1>Members</h1>
+              </div>
+              <div className="col-span-1 row-span-1 relative overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                  <h1>Donations</h1>
+              </div>
+              <div className="col-span-1 row-span-1 relative overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                  <h1>Life Groups</h1>
+              </div>
+          </div>
+      </section>
     </main>
   )
 }
