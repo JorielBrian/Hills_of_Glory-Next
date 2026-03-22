04020754d78ecb1c8189e1d94f438bf834cf9d90
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-22 15:35:53 +0800

    added a few contents

--- app/(dashboard)/dashboard/members/create/page.tsx
+++ app/(dashboard)/dashboard/members/create/page.tsx
@@ -1,8 +1,54 @@
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+  InputGroupText,
+  InputGroupTextarea,
+} from "@/components/ui/input-group"
+import { 
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger 
+} from "@/components/ui/dropdown-menu"
+
 const Create = () => {
   return (
     <main className='dashboard_main'>
         <div className='bg-white rounded-lg shadow-sm p-6'>
             <h2 className="text-2xl font-semibold mb-4">Add Member</h2>
+            <form action="" className="flex flex-col gap-4 p-4">
+                <InputGroup className="gap-2 p-5">
+                    <InputGroupInput placeholder="First Name" />
+                    <InputGroupInput placeholder="Middle Name" />
+                    <InputGroupInput placeholder="Last Name" />
+                </InputGroup>
+                <InputGroup className="gap-2 p-5">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <InputGroupButton
+                                variant="ghost"
+                            >
+                                Gender
+                            </InputGroupButton>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="bg-black/20 w-fit">
+                            <DropdownMenuGroup>
+                                <DropdownMenuItem>
+                                    <InputGroupText>Male</InputGroupText>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <InputGroupText>Female</InputGroupText>
+                                </DropdownMenuItem>
+                            </DropdownMenuGroup>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <InputGroupInput placeholder="Birth Date" />
+                    <InputGroupInput placeholder="Address" />
+                </InputGroup>
+            </form>
         </div>
     </main>
   )
