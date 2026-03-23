e3c5448225368d9b57ea6cd2b1b17130acfcc138
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-23 21:14:53 +0800

    fixed announcement and events arrangement

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ const Home = () => {
             <h1 className="text-xl 2xl:text-6xl p-8 2xl:w-1/2">TO HONOR GOD AND TO RAISE COMMITTED DISCIPLES</h1>
             <h2 className="h-fit text:lg 2xl:text-4xl text-[#fdc53a] px-10">Who are able to discple multitudes</h2>
         </section>
-        <section className="flex flex-col mt-20 2xl:w-1/2 2xl:p-20 2xl:mt-50">
+        <section className="flex flex-col md:flex-row mt-20 2xl:w-1/2 2xl:p-20 2xl:mt-50">
             <div>
                 <h3 className="py-3 text-md text-white font-[Century Gothic] font-bold 2xl:text-3xl">Announcements</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia doloribus fuga autem vel quasi porro. Placeat, quibusdam officia dolore culpa repudiandae doloribus eius doloremque ratione sequi cupiditate nostrum asperiores porro?</p>
