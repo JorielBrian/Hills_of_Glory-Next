ac60536126d1fb504c4f12ae4336065aed24b766
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-04-01 14:32:17 +0800

    fixed image

--- components/Dashboard/Sections/Header.tsx
+++ components/Dashboard/Sections/Header.tsx
@@ -8,7 +8,13 @@ const Header = () => {
     <header className="bg-[#b8bd9e]">
         <Link href="/">
             <div className="text_logo flex">
-                <Image src="/hog_logo.png" alt="logo" className="size-8 2xl:size-10" />
+                <Image 
+                    src="/hog_logo.png" 
+                    width={50}
+                    height={50}
+                    alt="logo" 
+                    className="size-8 2xl:size-10" 
+                />
                 <h1 className="px-1 text-lg 2xl:text-4xl text-[#fdc53a] font-bold content-center">Hills of Glory</h1>
                 <h1 className="px-1 text-lg 2xl:text-4xl content-center">Mabalacat</h1>
             </div>
