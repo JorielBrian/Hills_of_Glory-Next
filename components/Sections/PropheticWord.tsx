51cd34b8a6c891c12b69f14f2fab5a05175e1848
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-04-13 16:49:23 +0800

    made the Prophetic Word section responsive

--- components/Sections/PropheticWord.tsx
+++ components/Sections/PropheticWord.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const PropheticWord = () => {
   return (
-    <section className='flex flex-col gap-4 my-30 h-150 p-10 bg-[url(/divine_space_bg.png)] bg-cover bg-center text-green-900 text-center font-serif place-content-center'>
-        <p className='text-5xl'>
+    <section className='flex flex-col gap-4 my-30 py-2 md:py-5 xl:py-15 2xl:py-20 h-fit  bg-[url(/divine_space_bg.png)] bg-cover bg-center text-green-900 text-center font-serif place-content-center'>
+        <p className='text-2xl sm:text-3xl lg:text-4xl 2xl:text-5xl'>
             The Year of
         </p>
-        <h1 className='text-9xl'>
+        <h1 className='text-5xl sm:text-6xl md:text-7xl lg:text-8xl 2xl:text-9xl'>
             Divine Space
         </h1>
-        <h2 className='text-7xl'>
+        <h2 className='text-3xl sm:text-4xl lg:text-6xl 2xl:text-7xl'>
             2026
         </h2>
     </section>
