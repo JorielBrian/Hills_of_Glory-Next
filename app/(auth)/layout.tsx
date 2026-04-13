4d33b18435f242b2197baae11a9bbc0c3eccec6f
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-04-13 18:35:06 +0800

    fixed style

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { ReactNode } from 'react'
+import Image from 'next/image'
 
 const Layout = ({children}: { children: ReactNode }) => {
   return (
@@ -7,7 +8,7 @@ const Layout = ({children}: { children: ReactNode }) => {
       <div className="flex h-screen p-5 bg-black/60 items-center justify-center">
         <section className='flex flex-col w-4/5 2xl:w-1/4 h-fit p-5 items-center bg-black/20 rounded-xl'> 
             <Link href='/' className="text_logo flex mb-5 w-fit text-xl justify-center 2xl:text-3xl">
-                <img src="/hog_logo.png" alt="logo" className="size-10 2xl:size-15" />
+                <Image src="/hog_logo.png" alt="logo" width={60} height={60} className="size-10 2xl:size-15" />
                 <div className='sm:flex'>
                   <h1 className="text-[#fdc53a] content-center px-1">Hills of Glory</h1>
                   <h1 className="content-center px-1">Mabalacat</h1>
